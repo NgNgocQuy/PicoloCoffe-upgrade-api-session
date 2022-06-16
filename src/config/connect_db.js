be59525427ff71da61b3e812b751139fb06f9586
NgNgocQuy <46754367+NgNgocQuy@users.noreply.github.com>
2022-06-16 13:55:00 +0700

    Update connect_db.js

--- src/config/connect_db.js
+++ src/config/connect_db.js
@@ -4,7 +4,7 @@ async function connect() {
   
   try {
     console.log("Try connect to mongodb:");
-    await mongoose.connect("mongodb+srv://quys:0k1m3HaKOIjIU1X9@cluster0.uwau6.mongodb.net/coffee?retryWrites=true&w=majority", {
+    await mongoose.connect("mongodb+srv://<<>>:<<>>@cluster0.uwau6.mongodb.net/coffee?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
